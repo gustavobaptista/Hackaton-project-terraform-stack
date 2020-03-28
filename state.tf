80bef493fce59499ed3cf8d29b067ec5cc1907db
Gustavo Baptista <eng.gustavosilva@gmail.com>
2020-03-28 15:54:50 -0300

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackathon-fiap-1dvp-334099"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
